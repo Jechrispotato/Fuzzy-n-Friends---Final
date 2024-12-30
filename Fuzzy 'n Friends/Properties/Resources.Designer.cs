6c1336e21e5cb672c931845568f66020e44b66f7
Jecc <155385194+Jechrispotato@users.noreply.github.com>
2024-12-30 16:37:36 +0800

    Added Instructions

--- Fuzzy 'n Friends/Properties/Resources.Designer.cs
+++ Fuzzy 'n Friends/Properties/Resources.Designer.cs
@@ -373,6 +373,16 @@ namespace Fuzzy__n_Friends.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap fre {
+            get {
+                object obj = ResourceManager.GetObject("fre", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap guess_the_fruit {
             get {
                 object obj = ResourceManager.GetObject("guess_the_fruit", resourceCulture);
---
+++ Fuzzy 'n Friends/instructions.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace Fuzzy__n_Friends
+{
+    partial class instructions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // instructions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Fuzzy__n_Friends.Properties.Resources.fre;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.DoubleBuffered = true;
+            this.Name = "instructions";
+            this.Text = "instructions";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
